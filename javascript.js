436a19f8cca249b2b4e86074336f7767b04dafc1
somi batool <somibatool44@gmail.com>
2024-02-20 21:22:29 +0500

    new working

---
+++ javascript.js
@@ -0,0 +1,123 @@
+// console.log("somi batool")
+// console.log("i love javascript")
+// console.log("i am front end developer")
+// fullname:"batool";
+// age=12;
+// price=22.34;
+// x=null;
+// y=undefined;
+// radius=23;
+// isfollow=true;
+// let age =25;
+// const id=123;
+// let namefull="somi batoool"
+
+// console.log(age);
+// console.log(id);
+// console.log(namefull);
+// let isfollow= true;
+// const student ={
+//     fullname :"somi batool",
+//     age :12,
+//     ispass :"true",
+
+// };
+// const profile={
+//     username:"@somi batool",
+//     followers: 234,
+//     following:4,
+//     isfollow:true,
+//     bio: "hvjvkcutdycg",
+// };
+// we use this to check our datatype of an object 
+// console.log(typeof profile["followers"])
+// Artimetic operator
+// let a=3;
+// let b=4;
+// console.log("a=",a , "b=" ,b)
+// console.log("a + b =", a+b)
+// console.log("a - b =", a-b)
+// console.log("a * b =", a*b)
+// console.log("a / b =", a/b)
+// console.log("a % b =", a % b)
+// console.log("a ** b =", a ** b)
+
+
+// unary operators
+// let a=2;
+// let b=3;
+// console.log("a = ", a , "b=" , b)
+// a++;
+// console.log("a =", a);
+// b--;
+// console.log("b =", b);
+
+// Assignment operators
+// let a=6;
+// let b=2;
+// here this means that a=a+4;
+// a += 4;
+// console.log("a =", a);
+// COMPARISION OPERATORS
+
+// let a="5";
+// let b=5;
+// console.log("5===5", a===b);
+// console.log("2!=5", a!=b);
+// logical operators
+// let a=5;
+// let b=6;
+// let cond1 = a<b;
+// let cond2 = a>b;
+// console.log("cond1 && cond2 =", cond1 && cond2);
+// let color;
+// if(mode === "dark-mode"){
+//     color = "black";
+// }
+// let age = 17;
+// if(age > 18){
+//     console.log("you can vote");
+// }
+// let mode = "white";
+// let color;
+// if(mode =="dark"){
+//     color = "black"
+// }
+// else{
+//     color = "white";
+// }
+// console.log(color);
+// let a=13;
+
+// if(a%2==0){
+//     console.log("a is even number");
+// }
+// else{
+//     console.log("a is odd number");
+
+// }
+// let age = 16;
+// if(age>=18){
+//     console.log("they can vote");
+// }else{
+//     console.log("they can't vote")
+
+// }
+let mode = "pink";
+let color;
+if(mode=="dark"){
+    color = "black";
+} else if(mode=="pink"){
+    color="pink";
+}
+else if(mode=="red"){
+    color="red";
+}
+else if(mode=="yellow"){
+    color="yellow";
+}else{
+    color= "white";
+}
+console.log(color);
+
+
